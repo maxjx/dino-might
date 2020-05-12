31bb61cbaf9efeda9e00de8cee71f19880a71a6d
Maxnks <maxngkaishen@gmail.com>
2020-05-12 16:22:21 +0800

    Initialize project with assets

---
+++ milestone-1/Assets/playerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    public float horizontalMove = 0F;
+    public float runSpeed = 40F;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
