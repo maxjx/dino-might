04f2433f7bdac37fe7d3ae75eb9bdf4f43e3a38e
LimJunxue <junxue1@hotmail.com>
2020-05-14 23:08:02 +0800

    Added crouch animation and attack script
    Redid CharacterController2D script to follow Brackeys' more closely. Edited animator and made fireball attack and animations. Reorganised assets folder.

---
+++ milestone-1/Assets/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    public Transform firePoint;     // Point at which the bulletPrefab appears
+    public GameObject bulletPrefab; // Type of attack prefab
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Creates a bulletPrefab object at the position and rotation of the firePoint
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ milestone-1/Assets/Scripts/FireBall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject impactEffect; // A GameObject just to animate the impact effect at the point of collision
+    public float distance = 1;
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+        // Gives the rigidbody assigned to this GameObject a velocity of speed in the direction of right, which is the red axis
+        rb.velocity = transform.right * speed;
+    }
+
+    // Called when this GameObject enters the trigger collider of another GameObject, basically they collide
+    // hitInfo refers to the GameObject which is hit
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+        // code to damage enemy by calling enemy script component but theres no enemy created yet
+        // Enemy enemy = hitInfo.GetComponent<Enemy>();
+        // if (enemy != null)
+        // {
+        //     enemy.TakeDamage(damage);    // TakeDamage has to be a public method
+        // }
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void FixedUpdate() {
+        // Destroys GameObject after set displacement to free heap space and prevent undesired effects outside screen
+        if (Vector3.Distance(initialPosition, transform.position) > distance) {
+            Instantiate(impactEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
