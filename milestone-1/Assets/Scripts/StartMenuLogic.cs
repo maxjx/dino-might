5d1ba25155e78c6064830f4df79b1f92de393898
Maxnks <maxngkaishen@gmail.com>
2020-05-24 22:57:52 +0800

    Start Menu

---
+++ milestone-1/Assets/Scripts/StartMenuLogic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuLogic : MonoBehaviour {
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+}
