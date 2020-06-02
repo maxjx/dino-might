9210d2c2a5f70f64edaf6bba8ef8dce45a8f7494
Maxnks <maxngkaishen@gmail.com>
2020-06-02 18:17:27 +0800

    Add login logic

--- site/backend/Login.php
+++ site/backend/Login.php
@@ -14,7 +14,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully <br>";
 
 // Check the input username and comp
 $sql = "SELECT password FROM users WHERE username = '". $inputUsername. "'";
