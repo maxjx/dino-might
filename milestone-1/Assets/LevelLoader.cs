fb4a9056e0b3df2058e3a977a6820f21f390227b
Maxnks <maxngkaishen@gmail.com>
2020-06-04 12:42:50 +0800

    add animation between scene transition

---
+++ milestone-1/Assets/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Animator transition;
+
+    public void NextLevelAnimation(int index) {
+        StartCoroutine(LoadLevel(index));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        transition.SetTrigger("start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
