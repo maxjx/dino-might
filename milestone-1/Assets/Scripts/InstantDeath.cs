0bd73774d2a37733662b01d045e7d41b16988061
LimJunxue <junxue1@hotmail.com>
2020-06-07 01:10:43 +0800

    setup flying mob with pathfinding ai
    conditions from anystate should also have dead == false

---
+++ milestone-1/Assets/Scripts/InstantDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script to kill characters immediately
+public class InstantDeath : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        IHealth health = collider.GetComponent<IHealth>();
+        if (health != null)
+        {
+            health.Die();
+        }
+    }
+}
---
+++ milestone-1/Assets/Scripts/Mobs/Mob2Pathfinding.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Mob2Pathfinding : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 300f;
+    public float nextWaypointDistance = 3f;
+
+    //public Transform mobGFX;
+
+    Path path;
+    int currentWaypoint = 0;
+    //bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            //reachedEndOfPath = true;
+            return;
+        }
+        // else
+        // {
+        //     reachedEndOfPath = false;
+        // }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        rb.AddForce(force);
+
+        float sqdistance = (rb.position - (Vector2)path.vectorPath[currentWaypoint]).sqrMagnitude;
+
+        if (sqdistance < nextWaypointDistance * nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            transform.localScale = new Vector2(-4f, 4f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            transform.localScale = new Vector2(4f, 4f);
+        }
+    }
+}
