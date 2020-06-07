7914815a54ec6f96b3a8b73d159cd3a1443dc954
Maxnks <maxngkaishen@gmail.com>
2020-06-07 22:53:39 +0800

    pause menu

---
+++ milestone-1/Assets/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene("Start menu");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
