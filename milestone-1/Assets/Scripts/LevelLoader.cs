fbc052c9876ae6249aea34f7328ea68d0f03304d
Maxnks <maxngkaishen@gmail.com>
2020-06-08 16:39:38 +0800

    pause menu bug fix

--- milestone-1/Assets/Scripts/LevelLoader.cs
+++ milestone-1/Assets/Scripts/LevelLoader.cs
@@ -6,15 +6,22 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour {
 
     public Animator transition;
+    public GameObject screen;
+    public int sceneIndex;
 
     public void NextLevelAnimation(int index) {
         StartCoroutine(LoadLevel(index));
+        Debug.Log("coroutine called");
     }
-
+    public void NextLevelAnimationTest(int sceneIndex) {
+        StartCoroutine(LoadLevel(sceneIndex));
+        Debug.Log("coroutine called");
+    }
     IEnumerator LoadLevel(int levelIndex) {
         transition.SetTrigger("start");
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene(levelIndex);
+        transition.SetTrigger("End");
+        SceneManager.LoadSceneAsync(levelIndex);
         Debug.Log("passed loading of tut code");
         yield break;
     }
