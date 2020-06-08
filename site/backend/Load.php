5eb1ffcca4de831f2f6f05754a80a43789c0cc87
Maxnks <maxngkaishen@gmail.com>
2020-06-08 19:30:25 +0800

    Add loading feature

--- site/backend/Load.php
+++ site/backend/Load.php
@@ -4,8 +4,7 @@ $username = "id13880314_teamcook";
 $password = "XCgg8TruMc>0@Fkg";
 $dbname = "id13880314_dinomightdb";
 
-$inputUsername = $_POST["inputUsername"];
-$inputPassword = $_POST["inputPassword"];
+$playerId = $_POST["playerId"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,3 +14,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT * FROM loaddata WHERE id = '". $playerId. "'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  $rows = array();
+  while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+  }
+  echo json_encode($rows);
+} else {
+  echo "0";
+}
+$conn->close();
+
+?>
