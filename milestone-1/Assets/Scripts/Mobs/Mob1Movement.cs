0f7cd4b79fa8095b51054521551c77c9788d30ba
LimJunxue <junxue1@hotmail.com>
2020-06-12 18:23:53 +0800

    fix sum player death and mob spawn bugs

--- milestone-1/Assets/Scripts/Mobs/Mob1Movement.cs
+++ milestone-1/Assets/Scripts/Mobs/Mob1Movement.cs
@@ -33,8 +33,11 @@ public class Mob1Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        playerHealth = player.GetComponent<PlayerHealth>();
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            playerHealth = player.GetComponent<PlayerHealth>();
+        }
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 
@@ -117,7 +120,7 @@ public class Mob1Movement : MonoBehaviour
 
         }
         else
-        {   
+        {
             // else chasing but on platform edge, remain stationary until player goes out of chasing range
             // or attacking
             rb.velocity = Vector2.zero;
