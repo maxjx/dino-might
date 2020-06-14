9e1e612f43e55cdaa0d4d5462650dfa48c16e8d4
LimJunxue <junxue1@hotmail.com>
2020-06-15 00:53:58 +0800

    Started on mob3 movement
    sum green gem script changes

--- milestone-1/Assets/Scripts/GreenGem.cs
+++ milestone-1/Assets/Scripts/GreenGem.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 public class GreenGem : MonoBehaviour
 {
     public int addHealth = 1;
-    public Respawner respawner;
     private Animator animator;
-    private bool used = false;
-
-    void OnEnable()
-    {
-        used = false;
-    }
 
     void Start()
     {
@@ -21,17 +14,15 @@ public class GreenGem : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (!used && collider.CompareTag("Player"))
+        if (collider.CompareTag("Player"))
         {
             collider.GetComponent<PlayerHealth>().AddHealth(addHealth);
-            used = true;
             animator.SetTrigger("used");
         }
     }
 
-    void CallRespawner()
+    void CallDestroy()
     {
-        // Will set inactive, then set active after a few seconds
-        respawner.RespawnThing(gameObject);
+        Destroy(gameObject);
     }
 }
---
+++ milestone-1/Assets/Scripts/Mobs/Mob3.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob3 : MonoBehaviour
+{
+    public float speed = 1f;
+    public float roamingRange = 2f;     // Roams this distance away from the starting point and turns around
+
+    private Animator animator;
+    private Transform player;
+    private Rigidbody2D rb;
+    private Vector2 rightEnd;
+    private Vector2 leftEnd;
+    private bool movingRight = true;
+    private Vector2 targetPos;          // The target position is should move towards
+    private bool playerInRange = false;
+    private bool shooting = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        Vector2 startingpt = transform.position;    // Rigidbody2D's Y position is frozen to maintain a straight line of roaming
+
+        leftEnd = new Vector2(startingpt.x - roamingRange, startingpt.y);
+        rightEnd = new Vector2(startingpt.x + roamingRange, startingpt.y);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 currPos = transform.position;
+
+        if (shooting || playerInRange)
+        {
+            StartCoroutine(ShootLazer());
+        }
+        else
+        {
+            if (currPos.x <= leftEnd.x)
+            {
+                movingRight = true;
+                Flip();
+            }
+            else if (currPos.x >= rightEnd.x)
+            {
+                movingRight = false;
+                Flip();
+            }
+            // else current position is between the 2 roaming ends and should not change direction
+
+            // Determines its target position. Also accounts for the starting direction when currPos is btw the 2 roamEnds
+            if (movingRight)
+            {
+                targetPos = Vector2.MoveTowards(rb.position, rightEnd, speed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                targetPos = Vector2.MoveTowards(rb.position, leftEnd, speed * Time.fixedDeltaTime);
+            }
+
+            rb.MovePosition(targetPos);
+        }
+    }
+
+    IEnumerator ShootLazer()
+    {
+        shooting = true;
+        
+        animator.SetTrigger("attack");
+        yield return new WaitForSeconds(1f);
+
+        shooting = false;
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
