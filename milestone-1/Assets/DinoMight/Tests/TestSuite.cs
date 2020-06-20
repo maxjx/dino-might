6b4d59af5581c33ad1ae3ab8e6d586e13ceac438
Maxnks <maxngkaishen@gmail.com>
2020-06-20 19:04:33 +0800

    add file assembly

--- milestone-1/Assets/DinoMight/Tests/TestSuite.cs
+++ milestone-1/Assets/DinoMight/Tests/TestSuite.cs
@@ -6,11 +6,11 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class GameAssembly
+    public class TestSuite
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void GameAssemblySimplePasses()
+        public void TestSuiteSimplePasses()
         {
             // Use the Assert class to test conditions
         }
@@ -18,7 +18,7 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator GameAssemblyWithEnumeratorPasses()
+        public IEnumerator TestSuiteWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
