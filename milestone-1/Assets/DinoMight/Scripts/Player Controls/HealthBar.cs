45ca8b380d5c7a3db364ecb190feab33c20ac5b1
Maxnks <maxngkaishen@gmail.com>
2020-06-23 01:02:38 +0800

    added health bar

--- milestone-1/Assets/DinoMight/Scripts/Player Controls/HealthBar.cs
+++ milestone-1/Assets/DinoMight/Scripts/Player Controls/HealthBar.cs
@@ -7,16 +7,23 @@ public class HealthBar : MonoBehaviour {
     public Slider fill;
     public Gradient gradient;
     public Image fillColor;
+    public Text value;
+    private int currHealth;
+    private int maxHealth;
 
     public void setMaxHealth(int health) {
+        maxHealth = health;
         fill.maxValue = health;
-        fill.value = health;
+        fill.value = 0;
         fillColor.color = gradient.Evaluate(1f);
+        value.text = "0 / " + health.ToString();
     }
 
     public void setHealth(int health) {
-        fill.value = health;
+        currHealth = maxHealth - health;
+        fill.value = currHealth;
         fillColor.color = gradient.Evaluate(fill.normalizedValue);
+        value.text = currHealth.ToString() + " / " + maxHealth.ToString();
     }
 
 }
