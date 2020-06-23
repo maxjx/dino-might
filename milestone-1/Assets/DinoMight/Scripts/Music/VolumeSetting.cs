d7c11f433f517a3ddc7bd23f2fc2f613bdad4b92
Maxnks <maxngkaishen@gmail.com>
2020-06-24 00:38:58 +0800

    work in progress volume ctrl

---
+++ milestone-1/Assets/DinoMight/Scripts/Music/VolumeSetting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSetting : MonoBehaviour
+{
+    public void SetMusicVolume(float vol) {
+        AudioManager.Instance.SetMusicVolume(vol);
+    }
+
+    public void SetEffectsVolume(float vol) {
+        AudioManager.Instance.SetEffectsVolume(vol);
+    }
+}
