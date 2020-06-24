2b60124faffb6f6e4e374705ff6ad3c48632a726
LimJunxue <junxue1@hotmail.com>
2020-06-24 21:28:37 +0800

    half setup quest board and summary page

---
+++ milestone-1/Assets/DinoMight/Scripts/SummaryCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummaryCanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowAllChildren());
+
+    }
+
+    IEnumerator ShowAllChildren()
+    {
+        foreach (Transform child in this.transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
