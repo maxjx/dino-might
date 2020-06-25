05295bfe0cf194ffdfcab2199543cccb23b884fd
LimJunxue <junxue1@hotmail.com>
2020-06-25 21:08:09 +0800

    complete end to end quest 1
    change player health to 15 to match player non-prefab in boss lvl, otherwise value recorded will be out of bound

--- milestone-1/Assets/DinoMight/Scripts/QuestManager.cs
+++ milestone-1/Assets/DinoMight/Scripts/QuestManager.cs
@@ -8,18 +8,29 @@ public class QuestManager : MonoBehaviour
     public GameObject questToggleButton;
     public TextMeshProUGUI questBoardText;
     public GameObject questPrompt;
+    public bool isInTown = false;
+
     private int questNumber;
 
     // Start is called before the first frame update
     void Start()
     {
         questNumber = Global.questNumber;
-        if (questNumber == 0)
+
+        // If the first quest is not yet accepted && player is in town
+        if (questNumber == 0 && isInTown)
         {
             ShowQuestPrompt();
         }
+        else if ((questNumber%2 == 0) && isInTown)
+        {
+            ShowQuestPrompt();
+            ShowQuestButton();
+            UpdateQuestText();
+        }
         else
         {
+            // not in town
             ShowQuestButton();
             UpdateQuestText();
         }
@@ -35,21 +46,24 @@ public class QuestManager : MonoBehaviour
         questToggleButton.SetActive(true);
     }
 
+    // 3 quests, 3 after-quests
+    // Quest text must be updated at the start of every scene and when quest is accepted (but not when quest is done)
+    // Global quest number updates when quest is accepted or quest is done
     public void UpdateQuestText()
     {
         questNumber = Global.questNumber;
         switch (questNumber)
         {
-            case 0:
-                questBoardText.text = "Someone in the town needs your help!";
-                break;
             case 1:
+                // king quest
                 questBoardText.text = "Your boss is calling you and he sounds angry!\nFind him down the path to the west of the town, all the way down!";
                 break;
             case 2:
-                questBoardText.text = "";
+                // after king is defeated
+                questBoardText.text = "Nice job defeating that! Find the old guy in town for more instructions to get home!";
                 break;
             default:
+                questBoardText.text = "Someone in the town needs your help!";
                 break;
         }
     }
