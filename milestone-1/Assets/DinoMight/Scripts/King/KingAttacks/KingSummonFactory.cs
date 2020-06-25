54458d859ff3abeddac038eaeff835249811e824
Maxnks <maxngkaishen@gmail.com>
2020-06-25 15:13:55 +0800

    King SOLID + Factory

---
+++ milestone-1/Assets/DinoMight/Scripts/King/KingAttacks/KingSummonFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingSummonFactory : MonoBehaviour
+{
+    public void Summon(string type) {
+        switch (type) {
+            case "chicken":
+                gameObject.GetComponent<KingSummonerMob>().Summon();
+                break;
+            case "fist":
+                gameObject.GetComponent<KingSummonFist>().Summon();
+                break;
+        }
+    }
+}
