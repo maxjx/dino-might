29396e9d97e6ccf14d8acf04c380e6e21ae51e67
Junxue <junxue1@hotmail.com>
2020-06-30 01:29:22 +0800

    all trees to dontcrouch layer, fixed autotrigger dialogue

--- milestone-1/Assets/DinoMight/Tests/Playmode/DIalogueTestSuite.cs
+++ milestone-1/Assets/DinoMight/Tests/Playmode/DIalogueTestSuite.cs
@@ -3,26 +3,60 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEditor;
 
 namespace Tests
 {
     public class DIalogueTestSuite
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void DIalogueTestSuiteSimplePasses()
+        GameObject player;
+        GameObject npc;
+
+        [SetUp]
+        public void SetUp()
         {
-            // Use the Assert class to test conditions
+            player = new GameObject();
+            player.AddComponent<BoxCollider2D>();
+            player.GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
+            player.tag = "Player";
+            npc = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/DinoMight/Prefabs/NPC pack/Mask NPC.prefab"));
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator DIalogueTestSuiteWithEnumeratorPasses()
+        public IEnumerator PromptWhenPlayerIsInNPCCollider()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            player.transform.position = Vector3.zero;
+            npc.transform.position = Vector3.zero;
+
+            yield return new WaitForSeconds(1f);
+            player.transform.position = Vector3.zero;
+            npc.transform.position = Vector3.zero;
+            Debug.Log(npc.GetComponent<Collider2D>().bounds.Contains(player.transform.position));
+            
+            Debug.Log(npc.transform.GetChild(0).gameObject.name);
+
+            // player.transform.position = new Vector3(0.865f, 0.427f, 0);
+            // yield return new WaitForSeconds(0.1f);
+
+            Assert.IsTrue(npc.transform.GetChild(0).gameObject.activeInHierarchy);
+        }
+
+        [UnityTest]
+        public IEnumerator NoPromptWhenPlayerIsNotInNPCCollider()
+        {
+            player.transform.position = Vector3.zero;
+            npc.transform.position = new Vector3(100, 100, 100);
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.IsFalse(npc.transform.GetChild(0).gameObject.activeInHierarchy);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GameObject.Destroy(player);
+            GameObject.Destroy(npc);
         }
     }
 }
