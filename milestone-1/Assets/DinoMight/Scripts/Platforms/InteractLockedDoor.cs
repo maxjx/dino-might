a6c4289d775f48ba48dff57c9afe6519e6f08643
Maxnks <maxngkaishen@gmail.com>
2020-07-02 01:26:22 +0800

    added locked doors and key

---
+++ milestone-1/Assets/DinoMight/Scripts/Platforms/InteractLockedDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractLockedDoor : Interact
+{
+    [SerializeField] private Key.KeyType keyType;
+    [SerializeField] private GameObject noKeyMessage;
+
+    protected override void TriggerAction()
+    {
+        LockedDoor door = gameObject.GetComponent<LockedDoor>();
+        if (!door.isCurrentlyLocked())
+        {
+            return;
+        }
+
+        KeyHolder keyHolder = player.gameObject.GetComponent<KeyHolder>();
+        if (keyHolder.KeyTypeIsPresent(keyType))
+        {
+            gameObject.GetComponent<LockedDoor>().Toggle();
+            keyHolder.DeleteKey(keyType);
+        }
+        else
+        {
+            StartCoroutine(ErrorMessage());
+        }
+    }
+
+    private IEnumerator ErrorMessage()
+    {
+        noKeyMessage.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        noKeyMessage.SetActive(false);
+    }
+}
---
+++ milestone-1/Assets/DinoMight/Scripts/Platforms/Key.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Key : MonoBehaviour
+{
+    [SerializeField] private KeyType keyType;
+
+    public enum KeyType {
+        Yellow
+    }
+
+    public KeyType getKeyType() {
+        return keyType;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+
+        KeyHolder keyHolder = other.gameObject.GetComponent<KeyHolder>();
+
+        if (keyHolder == null)
+        {
+            return;
+        }
+
+        keyHolder.AddKey(keyType);
+        Destroy(gameObject);
+    }
+}
