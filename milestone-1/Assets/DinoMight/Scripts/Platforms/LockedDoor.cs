39bbdc765fdd0860dec1c60d0fd11b048550d5c4
Maxnks <maxngkaishen@gmail.com>
2020-07-02 01:26:22 +0800

    added locked doors and key

---
+++ milestone-1/Assets/DinoMight/Scripts/Platforms/LockedDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class LockedDoor : MonoBehaviour
+{
+    private bool isLocked = true;
+    [SerializeField] GameObject teleportTo;
+
+    public void Toggle()
+    {
+        isLocked = !isLocked;
+    }
+
+    private void OnTriggerStay2D(Collider2D player) {
+        if (isLocked)
+        {
+            return;
+        }
+
+        player.transform.position = teleportTo.transform.position;
+    }
+
+    public bool isCurrentlyLocked() {
+        return isLocked;
+    }
+}
