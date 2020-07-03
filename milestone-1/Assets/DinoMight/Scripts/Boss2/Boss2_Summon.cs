6a6742d0116f2c159abc3f7667b3bd6ec6a51416
Junxue <junxue1@hotmail.com>
2020-07-03 20:44:10 +0800

    boss 2 done......

--- milestone-1/Assets/DinoMight/Scripts/Boss2/Boss2_Summon.cs
+++ milestone-1/Assets/DinoMight/Scripts/Boss2/Boss2_Summon.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Boss2_Summon : StateMachineBehaviour
 {
+    Transform tf;
+    Vector3 offset_x;
+    Vector3 lightningOffset_y;
+    Vector3 cloudOffset_y;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        tf = animator.transform;
+        offset_x = new Vector3(3.8f, 0, 0);
+        if (tf.rotation.y < 0)
+        {
+            offset_x.x = -offset_x.x;
+        }
+        cloudOffset_y = new Vector3(0, 3.5f, 0);
+        ObjectPooler.Instance.SpawnFromPool("Cloud", tf.position + offset_x + cloudOffset_y, tf.rotation);
+        ObjectPooler.Instance.SpawnFromPool("Cloud", tf.position + (offset_x*2) + cloudOffset_y, tf.rotation);
+        ObjectPooler.Instance.SpawnFromPool("Cloud", tf.position + (offset_x*3) + cloudOffset_y, tf.rotation);
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,10 +31,13 @@ public class Boss2_Summon : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        lightningOffset_y = new Vector3(0, 1.2f, 0);
+        ObjectPooler.Instance.SpawnFromPool("Lightning", tf.position + offset_x + lightningOffset_y, tf.rotation);
+        ObjectPooler.Instance.SpawnFromPool("Lightning", tf.position + (offset_x*2) + lightningOffset_y, tf.rotation);
+        ObjectPooler.Instance.SpawnFromPool("Lightning", tf.position + (offset_x*3) + lightningOffset_y, tf.rotation);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
