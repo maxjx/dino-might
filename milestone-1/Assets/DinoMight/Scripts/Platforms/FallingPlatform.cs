0b9ed885812ce5c971d88d3a9990bbe4daf28e07
Maxnks <maxngkaishen@gmail.com>
2020-07-07 15:42:12 +0800

    Complete snowy minimap

---
+++ milestone-1/Assets/DinoMight/Scripts/Platforms/FallingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FallingPlatform : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float waitTime = 1.5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Drop());
+        }
+    }
+
+    private IEnumerator Drop()
+    {
+        yield return new WaitForSeconds(waitTime);
+        rb.isKinematic = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
