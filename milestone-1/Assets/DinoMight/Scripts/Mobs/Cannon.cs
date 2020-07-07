bf181cec7ff2d8668944a7b0c25d6199d3fa423f
Maxnks <maxngkaishen@gmail.com>
2020-07-07 17:05:46 +0800

    add canon enemy

---
+++ milestone-1/Assets/DinoMight/Scripts/Mobs/Cannon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    private float timer;
+    [SerializeField] private GameObject prefab;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && timer > 4f)
+        {
+            timer = 0f;
+            float pos = other.gameObject.transform.position.x;
+            Fire(pos);
+        }
+    }
+
+    private void Fire(float pos)
+    {
+        float selfPos = pos - gameObject.transform.position.x;
+        float xVelocity = selfPos/1.79f;
+        GameObject bomb = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
+
+        bomb.GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, 7.67f);
+    }
+}
