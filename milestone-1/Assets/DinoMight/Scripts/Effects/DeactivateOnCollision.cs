870e44cebefb38d43d4f0226aaf88666468d4661
Junxue <junxue1@hotmail.com>
2020-07-10 15:11:15 +0800

    arc 2 levels

---
+++ milestone-1/Assets/DinoMight/Scripts/Effects/DeactivateOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateOnCollision : Deactivate
+{
+    public LayerMask collisionLayers;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collisionLayers == (collisionLayers | (1 << collider.gameObject.layer)))
+            gameObject.GetComponent<Animator>().SetTrigger("hit");
+    }
+}
---
+++ milestone-1/Assets/DinoMight/Scripts/MiscBehaviour/Stationary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stationary : StateMachineBehaviour
+{
+    Rigidbody2D rb;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       rb.velocity = Vector2.zero;
+    }
+}
