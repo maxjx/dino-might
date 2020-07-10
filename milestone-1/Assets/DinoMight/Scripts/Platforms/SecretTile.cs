7fc15c9cc1f7e25b000cf3aca3d3b820af058634
Maxnks <maxngkaishen@gmail.com>
2020-07-10 16:16:08 +0800

    add scene adjustments

---
+++ milestone-1/Assets/DinoMight/Scripts/Platforms/SecretTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretTile : MonoBehaviour
+{
+    public GameObject tiles;
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D other)
+    {  
+        tiles.SetActive(true);
+    }
+}
