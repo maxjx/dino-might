2732f64ad23195cfa001e17290fe4f3dff6f88ea
Junxue <junxue1@hotmail.com>
2020-07-11 21:02:30 +0800

    add fairy in m scenes, ai and dialogue canvas tests

--- milestone-1/Assets/DinoMight/Tests/Playmode/DIalogueTestSuite.cs
+++ milestone-1/Assets/DinoMight/Tests/Playmode/DIalogueTestSuite.cs
@@ -11,16 +11,22 @@ namespace Tests
     {
         GameObject player;
         GameObject npc;
+        DialogueManager dm;
 
         [SetUp]
         public void SetUp()
         {
-            player = new GameObject();
-            player.transform.position = Vector3.zero;
-            player.AddComponent<BoxCollider2D>();
-            player.GetComponent<BoxCollider2D>().size = new Vector2(1, 2);
-            player.tag = "Player";
+            // player = new GameObject();
+            // player.transform.position = Vector3.zero;
+            // player.AddComponent<BoxCollider2D>();
+            // player.GetComponent<BoxCollider2D>().size = new Vector2(1, 2);
+            // player.tag = "Player";
+            player = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/DinoMight/Prefabs/Character (pf).prefab"));
             npc = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/DinoMight/Prefabs/NPC pack/Kiddo NPC.prefab"));
+            dm = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/DinoMight/Prefabs/NPC pack/DialogueManager.prefab")).GetComponent<DialogueManager>();
+            dm.player = player;
+            dm.dialogueCanvasesTaskValue = new List<int>() { 2, 4 };
+            dm.recordConvo = false;
         }
 
         [Test]
@@ -35,29 +41,52 @@ namespace Tests
         [Test]
         public void CorrectCanvasWhenQuestIsLessThanValues()
         {
-            DialogueManager dm = new GameObject().AddComponent<DialogueManager>();
-            dm.player = player;
-            dm.dialogueCanvasesTaskValue = new List<int>(){ 2, 4 };
             Global.questNumber = 1;
-
             Assert.AreEqual(0, dm.GetCanvasIndex());
         }
 
+        [Test]
+        public void CorrectCanvasWhenQuestIsEqualValue()
+        {
+            Global.questNumber = 2;
+            Assert.AreEqual(0, dm.GetCanvasIndex());
+        }
+        
+        [Test]
+        public void CorrectCanvasWhenQuestIsInBetweenValues()
+        {
+            Global.questNumber = 3;
+            Assert.AreEqual(1, dm.GetCanvasIndex());
+        }
+
+        [Test]
+        public void CorrectCanvasWhenQuestIsMoreThanValues()
+        {
+            Global.questNumber = 5;
+            Assert.AreEqual(1, dm.GetCanvasIndex());
+        }
+
+        [UnityTest]
+        public IEnumerator PromptWhenPlayerIsInNPCCollider()
+        {
+            player.transform.position = Vector3.zero;
+            npc.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.IsTrue(npc.transform.GetChild(0).gameObject.activeInHierarchy);
+        }
+        
         // [UnityTest]
-        // public IEnumerator PromptWhenPlayerIsInNPCCollider()
+        // public IEnumerator NoPromptWhenPlayerIsInNPCColliderAndCPressed()
         // {
         //     player.transform.position = Vector3.zero;
         //     npc.transform.position = Vector3.zero;
-
-        //     yield return new WaitForSeconds(0.1f);
-        //     player.transform.position = Vector3.zero;
-        //     npc.transform.position = Vector3.zero;
-        //     Debug.Log(npc.GetComponent<Collider2D>().bounds.Contains(player.transform.position));
             
-        //     Debug.Log(npc.transform.GetChild(0).gameObject.name);
+        //     yield return new WaitForSeconds(0.1f);
+        //     npc.GetComponent<DialogueTrigger>().TriggerKeysPressed().Returns(true);
+        //     yield return new WaitForSeconds(0.1f);
 
-        //     Assert.IsTrue(npc.transform.GetChild(0).gameObject.activeInHierarchy);
-        //     //Assert.IsTrue(player.GetComponent<BoxCollider2D>().isTrigger);
+        //     Assert.IsFalse(npc.transform.GetChild(0).gameObject.activeInHierarchy);
         // }
 
         [UnityTest]
@@ -76,6 +105,7 @@ namespace Tests
         {
             GameObject.Destroy(player);
             GameObject.Destroy(npc);
+            GameObject.Destroy(dm);
         }
     }
 }
