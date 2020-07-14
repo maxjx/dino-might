bfd8c58bde1e0d88695ebf752156e1a52468da08
Maxnks <maxngkaishen@gmail.com>
2020-07-14 13:51:49 +0800

    add volume adjustment and prefab

--- milestone-1/Assets/DinoMight/Scripts/Music/VolumeSetting.cs
+++ milestone-1/Assets/DinoMight/Scripts/Music/VolumeSetting.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSetting : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider musicVolume;
+    public Slider effectsVolume;
+
+    private void Start()
+    {
+        musicVolume.value = Global.musicVolume;
+        effectsVolume.value = Global.effectsVolume;
+    }
 
     public void SetMusicVolume(float vol) {
+        // The value saved to global is between 0.01 and 1
+        Global.musicVolume = vol;
+
         // Convert the volume slider to a linear adjustment since decibels is in log10
         vol = Mathf.Log10(vol) * 40;
         audioMixer.SetFloat("MusicVol", vol);
     }
 
     public void SetEffectsVolume(float vol) {
+        // The value saved to global is between 0.01 and 1
+        Global.effectsVolume = vol;
+        
         // Convert the volume slider to a linear adjustment since decibels is in log10
         vol = Mathf.Log10(vol) * 40;
         audioMixer.SetFloat("EffectsVol", vol);
