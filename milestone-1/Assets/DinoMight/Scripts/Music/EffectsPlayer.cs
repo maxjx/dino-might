4dfc3ddfecba965ece89644b2cef95edb3573e97
Junxue <junxue1@hotmail.com>
2020-07-21 01:13:23 +0800

    meditation done

---
+++ milestone-1/Assets/DinoMight/Scripts/Music/EffectsPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsPlayer : MonoBehaviour
+{
+    public AudioClip music;
+    private bool playing = false;       // to prevent playing the same track more than once
+
+    public void PlayEffect()
+    {
+        if (!playing)
+        {
+            AudioManager.Instance.PlayEffect(music);
+            playing = true;
+        }
+    }
+
+    public void UnpauseEffect()
+    {
+        if (!playing)
+        {
+            AudioManager.Instance.UnpauseEffect();
+            playing = true;
+        }
+    }
+
+    public void PauseEffect()
+    {
+        if (playing)
+        {
+            AudioManager.Instance.PauseEffect();
+            playing = false;
+        }
+    }
+
+    public void StopEffect()
+    {
+        AudioManager.Instance.StopEffect();
+        playing = false;
+    }
+
+}
