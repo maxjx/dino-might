8536aaedc820bd7e548eebcc94ca9e46898c0151
Junxue <junxue1@hotmail.com>
2020-07-22 01:12:29 +0800

    hashset to list, summary tips

--- milestone-1/Assets/DinoMight/Scripts/Summary/SummaryTips.cs
+++ milestone-1/Assets/DinoMight/Scripts/Summary/SummaryTips.cs
@@ -21,11 +21,11 @@ public class SummaryTips : MonoBehaviour
         // Assume max value is 2 points
         if (Global.priorities.Count < 2)
         {
-            if (!Global.priorities.Contains("1eatlunch"))
+            if (!Global.priorities.Contains("eatlunch"))
             {
                 result += "Skipping meals is wrecks your body and mind! Use mealtime to relax and reflect on your day.\n";
             }
-            if (!Global.priorities.Contains("1planfuture"))
+            if (!Global.priorities.Contains("planfuture"))
             {
                 result += "Always have a goal in mind, be it short or long term. It keeps you focused.\n";
             }
@@ -38,14 +38,14 @@ public class SummaryTips : MonoBehaviour
 
         if (Global.challenges.Count < 2)
         {
-            if (!Global.challenges.Contains("2ownup"))
+            if (!Global.challenges.Contains("ownup"))
             {
                 result += "Talk to people about your problems to take things off your chest, and face setbacks with a positive attitude.\n";
             }
-            // if (!Global.challenges.Contains("2"))
-            // {
-            //     result += "";
-            // }
+            if (!Global.challenges.Contains("journal"))
+            {
+                result += "A journal is a good friend in need when you are facing problems, or to organise your thoughts. Practise writing down your day and reflect on what was good or bad.";
+            }
             result += "Embrace challenges as there will be better days!\n\n";
         }
         else
@@ -55,14 +55,14 @@ public class SummaryTips : MonoBehaviour
 
         if (Global.habits.Count < 2)
         {
-            // if (!Global.habits.Contains("3workout"))
-            // {
-            //     result += "";
-            // }
-            // if (!Global.habits.Contains("3hwbreak"))
-            // {
-            //     result += "";
-            // }
+            if (!Global.habits.Contains("workout"))
+            {
+                result += "Do create a positive habit of exercising. Even a short 10 minute stretching in the morning will make you feel so much better throughout the day and keep you performing tip top!";
+            }
+            if (!Global.habits.Contains("hwbreak"))
+            {
+                result += "Take a break! Remember to schedule breaks in between your work. The Pomodoro Technique calls for work sessions of 25mins followed by a 5min break, with a 15min break at least once every two hours.";
+            }
             result += "Regular sleeping and eating patterns are important to make your brain strong. Remember to get in some exercise too!";
         }
         else
