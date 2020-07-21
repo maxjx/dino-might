33ab45b89d1ff5849aba1d15f9d07c939df01580
Maxnks <maxngkaishen@gmail.com>
2020-07-21 19:14:36 +0800

    add sound effects

---
+++ milestone-1/Assets/DinoMight/Scripts/Player Controls/PlayerSFXControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSFXControl : MonoBehaviour
+{
+    public AudioClip kick;
+    public AudioClip fireball;
+    public AudioClip dash;
+    public AudioClip jump;
+    public AudioClip damage;
+    public AudioClip cure;
+
+    public void playKick()
+    {
+        AudioManager.Instance.PlayEffect(kick);
+    }
+
+    public void playFireball()
+    {
+        AudioManager.Instance.PlayEffect(fireball);
+    }
+
+    public void playDash()
+    {
+        AudioManager.Instance.PlayEffect(dash);
+    }
+
+    public void playJump()
+    {
+        AudioManager.Instance.PlayEffect(jump);
+    }
+
+    public void playDamage()
+    {
+        AudioManager.Instance.PlayEffect(damage);
+    }
+
+    public void playCure()
+    {
+        AudioManager.Instance.PlayEffect(cure);
+    }
+}
