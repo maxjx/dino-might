980af36807a675face84b59e5c2334f09a86eb1d
Junxue <junxue1@hotmail.com>
2020-07-22 17:03:11 +0800

    selected button selective on any key

--- milestone-1/Assets/DinoMight/Scripts/Menu/SetSelectedGO.cs
+++ milestone-1/Assets/DinoMight/Scripts/Menu/SetSelectedGO.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SetSelectedGO : MonoBehaviour
 {
     public bool onEnable = true;
+    public bool onAnyKey = false;
 
     void OnEnable()
     {
         if (onEnable)
         {
             EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (onAnyKey)
+        {
+            if (Input.anyKeyDown)
+            {
+                gameObject.GetComponent<Button>().onClick.Invoke();
+            }
         }
     }
 }
