abfb18d7258bff2adc4efcede29e414ab4c63d6c
Junxue <junxue1@hotmail.com>
2020-07-22 18:30:56 +0800

    town bugs

---
+++ milestone-1/Assets/DinoMight/Scripts/Platforms/DisableCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCollider : MonoBehaviour
+{
+    public int questNumber;     // quest number less than this value will not disable the collider
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (questNumber >= Global.questNumber)
+        {
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
